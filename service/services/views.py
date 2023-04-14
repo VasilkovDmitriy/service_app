8c7f571df56021efd30eed2a34b62c6f1dd28f38
VasilkovDmitriy <vasilkovit2014@gmail.com>
2023-04-14 18:30:16 +0200

    add plan to subscription response

--- service/services/views.py
+++ service/services/views.py
@@ -1,9 +1,16 @@
+from django.db.models import Prefetch
 from rest_framework.viewsets import ReadOnlyModelViewSet
 
+from clients.models import Client
 from services.models import Subscription
 from services.serializers import SubscriptionSerializer
 
 
 class SubscriptionView(ReadOnlyModelViewSet):
-    queryset = Subscription.objects.all()
+    queryset = Subscription.objects.prefetch_related(
+        'plan',
+        Prefetch(
+            'client', queryset=Client.objects.select_related('user').only('company_name', 'user__email')
+        )
+    )
     serializer_class = SubscriptionSerializer
