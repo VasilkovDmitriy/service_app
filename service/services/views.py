330d9ea2f0f5f64d69fb25004e2ba93b8f218b8c
Dmitriy Vasilkov <vasilkovit2014@gmail.com>
2023-04-16 20:50:10 +0200

    calculate subscribe plan to celery task

--- service/services/views.py
+++ service/services/views.py
@@ -10,8 +10,6 @@ class SubscriptionView(ReadOnlyModelViewSet):
     queryset = Subscription.objects.prefetch_related(
         'plan',
         Prefetch('client', queryset=Client.objects.select_related('user').only('company_name', 'user__email'))
-    ).annotate(
-        price=F('service__full_price') - F('service__full_price') * F('plan__discount_percent') / 100
     )
     serializer_class = SubscriptionSerializer
 
